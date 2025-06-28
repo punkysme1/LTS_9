560aa7885a2e6daae47de169fbde37313cef4c4e
tppkpqom <tppkp.qomaruddin@gmail.com>
2025-06-28 16:02:29 +0700

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./{components,services,App,index}.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- INI PERBAIKANNYA
   ],
   theme: {
     extend: {
