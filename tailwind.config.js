6832068bf3d81c9b6837db2752d2c9ed128598fd
tppkpqom <tppkp.qomaruddin@gmail.com>
2025-07-08 18:18:46 +0700

    perbaikan lagi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Path yang lebih spesifik dan efisien
+    "./**/*.{js,ts,jsx,tsx}", // <-- INI PERBAIKAN FINAL. Memindai semua file di semua folder dari root.
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+        'serif': ['Merriweather', 'serif'],
+      },
+      colors: {
+        'brand-dark': '#1a2c42',
+        'brand-light': '#f0e9e0',
+        'brand-accent': '#c5a572',
+        'brand-accent-dark': '#a88c5f',
+      }
+    },
   },
   plugins: [],
 }
